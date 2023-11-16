7bfdf96bdd4f96c28b30a0eb592d40a8ffeefd70
Christopher Forti <christopher.forti.79@gmail.com>
2023-11-16 17:27:08 -0500

    Wrote itempotent scripts for node, and npm install

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-# Npm should be installed
+# Node should be installed
 
-if (npm version)
+if (which node)
+then 
+ echo "node is installed"
+else
+ echo "installing node"
+ brew install node
+fi
+
+# https://github.com/nodesource/distributions#deb
+
+
+# Npm should be installed
+# gets installed with brew install node
+
+if (npm -version)
+then 
+ echo "npm is installed"
+else
+ echo "installing npm"
+ brew upgrade node
+fi
+
+# npm install
+
+# curl localhost:3000 
+# node app.js
