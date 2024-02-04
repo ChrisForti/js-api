6f612dc9d91fd08c06f5b6629c287d3e999baa3c
Christopher Forti <christopher.forti.79@gmail.com>
2024-02-03 21:48:52 -0500

    Started playing with using express static.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use("./assets", express.static("assets"));
+
 app.get("/", (req, res) => {
   res.send(
     "<h1>Carefully watch your thoughts, for they become your words. Manage and watch your words, for they will become your actions. ~ Mahatma Gandhi ~ </h1><a href='/info'>Info page</a>"
