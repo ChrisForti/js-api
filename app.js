1ac5e6436e5eb33a0ce03c336b0158384aa380c7
Christopher Forti <christopher.forti.79@gmail.com>
2024-02-18 18:12:38 -0500

    Styling changes in static assets.

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send(
-    "<h1>Carefully watch your thoughts, for they become your words. Manage and watch your words, for they will become your actions. ~ Mahatma Gandhi ~ </h1><a href='/info'>Info page</a>"
-  );
+  res.send("<h1>Homepage</h1><a href='/info'>Info page</a>");
 });
 
 app.get("/info", (req, res) => {
